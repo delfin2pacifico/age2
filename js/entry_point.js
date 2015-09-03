b808e319851d6894f7443d3291cfc45aa788f567
delfin2pacifico <delfin2pacifico@gmail.com>
2015-09-03 19:44:57 +0200

    model moveBy and moveTo

--- js/entry_point.js
+++ js/entry_point.js
@@ -13,5 +13,20 @@
     model.moveElmTo = function (elm, positionXY) {
         elm.style.left = positionXY.x + 'px';
         elm.style.top = positionXY.y + 'px';
-    }
+    };
+
+    var pointerCros = {
+        "verticalLine": document.createElement('div'),
+        "horizontalLine": document.createElement('div')
+    };
+
+    var styleSheet = document.createElement('style');
+    document.head.appendChild(styleSheet);
+
+    styleSheet = styleSheet.sheet;
+    styleSheet.addRule('.verticalLine', 'position:absolute');
+
+
+    console.log();
+
 }());
