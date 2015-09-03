570f50704ccfb04b09e04ab5f13f1c99b48ac3be
delfin2pacifico <delfin2pacifico@gmail.com>
2015-09-03 20:26:23 +0200

    cros lines added

--- js/entry_point.js
+++ js/entry_point.js
@@ -23,10 +23,23 @@
     var styleSheet = document.createElement('style');
     document.head.appendChild(styleSheet);
 
-    styleSheet = styleSheet.sheet;
-    styleSheet.addRule('.verticalLine', 'position:absolute');
+    styleSheet.sheet.insertRule('.verticalLine {position: absolute; width: 1px; height: 100%; border-left: 1px dotted #555;}', 0);
+    styleSheet.sheet.insertRule('.horizontalLine {position: absolute; width: 100%; height: 1px; border-top: 1px dotted #555;}', 0);
 
 
-    console.log();
+    pointerCros.verticalLine.classList.add('verticalLine');
+    pointerCros.horizontalLine.classList.add('horizontalLine');
+
+    document.body.appendChild(pointerCros.verticalLine);
+    document.body.appendChild(pointerCros.horizontalLine);
+
+
+    window.addEventListener('mousemove', function (eventObj) {
+
+        pointerCros.verticalLine.style.left = eventObj.clientX + 'px';
+        pointerCros.horizontalLine.style.top = eventObj.clientY + 'px';
+
+    })
+
 
 }());
